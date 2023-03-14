d3c58e0d49934321ab730de960d6031474e3d8e0
AartiPundalik <aartipundalik68@gmail.com>
2023-03-14 17:45:27 +0530

    [Aarti] Add : Checking Even Or Odd Number

---
+++ LogicalPracticeProblems/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPracticeProblems
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            EvenOrOdd Checkevenorodd = new EvenOrOdd();
+            Checkevenorodd.Checkevenorodd();
+            Console.ReadLine();
+        }
+    }
+
+}
+    
+
